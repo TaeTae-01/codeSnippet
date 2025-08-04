ee9118d0793c47419d9ca390fb51e20ff9a35f63
TaeTae-01 <ctj0999@naver.com>
2025-08-05 01:27:01 +0900

    prettier setting

---
+++ settings/prettier.config.js
@@ -0,0 +1,15 @@
+// 또는 .prettierrc.json 파일
+{
+  "singleQuote": false,
+  "semi": true,
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "printWidth": 80,
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "endOfLine": "lf",
+  "jsxSingleQuote": false,
+  "jsxBracketSameLine": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "css"
+}
